99600f924c7ede16b185fdc7510a5c4e550052c0
Stas Dukach <stasdukach@gmail.com>
2021-05-25 14:38:19 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.1024668732f8481b278e42a307ce91c0.js"
 );
 
 self.addEventListener('message', (event) => {
